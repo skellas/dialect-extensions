94dbf6f4d467e11315bf5e221bf2e0f6df2b083c
Seth Kellas <sjkellas@gmail.com>
2021-10-08 19:34:52 -0400

    Spring initializer creation step

---
+++ src/main/java/com/sethkellas/dialectextensions/DialectExtensionsApplication.java
@@ -0,0 +1,13 @@
+package com.sethkellas.dialectextensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialectExtensionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialectExtensionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sethkellas/dialectextensions/DialectExtensionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sethkellas.dialectextensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialectExtensionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
